2a11175ee64b65fa2e02bdff4c8f268e6194f6f3
Mena Mahesh <mena.mahi0506@gmail.com>
2024-12-02 18:13:11 +0530

    First Commit for Demo Project

---
+++ src/MyNewTest.java
@@ -0,0 +1,17 @@
+
+public class MyNewTest {
+
+	public static void main(String[] args) {
+		
+		String hungry = "Yes" ;
+		if (hungry == "Yes")
+		{
+			System.out.println("Eat Something");
+		}
+		else
+		{
+			System.out.println("Do your work");
+		}
+	}
+
+}
